c4cc5b457ac86921c205dc654da5fb5583a0fcd8
JayProck <jprochnow1@gmail.com>
2018-09-01 06:07:13 -0400

    Initial commit

---
+++ src/main/java/com/bitbus/fantasyprep/FantasyPrepApplication.java
@@ -0,0 +1,12 @@
+package com.bitbus.fantasyprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyPrepApplication.class, args);
+	}
+}
